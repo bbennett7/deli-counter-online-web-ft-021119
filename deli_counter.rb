6c765cf0742363723d6a197e3b4609f0c9953423
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-12 23:48:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,11 @@ def take_a_number(array, name)
   array << name 
   puts "Welcome, #{array[-1]}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+  if array.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    
+  end
+end
