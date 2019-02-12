27460a8ea51ff50b40476ecc40ce14857c656d56
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-12 23:41:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ katz_deli = []
 
 def line(array)
   if array.length == 0 
-    "The line is currently empty."
+    puts "The line is currently empty."
   else 
     line_string = "The line is currently:"
     line_string << array.each_with_index{|name, index| puts " #{index}. #{name}"}
-    line_string
+    puts line_string
   end
 end
