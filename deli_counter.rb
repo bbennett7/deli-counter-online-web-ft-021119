b992d54ca5e3762825f69c8357759571e3792af6
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-12 23:36:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,6 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
-    array.each_with_index do |name, index|
-      puts "The line is currently: #{index}."
-    end
+    puts "The line is currently: #{array.join("#{index}.")}"
   end
 end
